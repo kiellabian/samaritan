ca76538044d8637708def7b2566b90735ec7ecf4
Julia Menchavez <jcmenchavez@gmail.com>
2015-05-19 17:38:30 +0800

    plot learning curves

--- classifier.py
+++ classifier.py
@@ -1,10 +1,12 @@
 from operator import itemgetter
 import random
 
+import matplotlib.pyplot as plt
+
 from nltk import classify
 
 from sklearn import cross_validation
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, f1_score
 
 
 class Classifier:
@@ -29,17 +31,21 @@ class Classifier:
         else:
             self.classifier = self.classifier.train(train_set)
 
-    def test(self):
-        accuracy = classify.accuracy(self.classifier, self.test_set)
+    def test(self, test_set=None):
+        if not test_set:
+            test_set = self.test_set
+        accuracy = classify.accuracy(self.classifier, test_set)
         print 'Accuracy: ' + str(accuracy)
 
         features = []
         labels = []
-        for (feat, label) in self.feature_set:
+        for (feat, label) in test_set:
             features.append(feat)
             labels.append(label)
         predictions = self.classifier.classify_many(features)
         print classification_report(predictions, labels)
+        fscore = f1_score(predictions, labels)
+        return accuracy, fscore
 
     def cross_validate(self, n_folds=2):
         best_accuracy = 0.0
@@ -57,3 +63,29 @@ class Classifier:
                 best_classifier = classifier
                 best_accuracy = accuracy
         return {'classifier': best_classifier, 'accuracy': accuracy}
+
+    def plot_curves(self, start_size, inc_size):
+        data_size = start_size
+        data_sizes = []
+        accuracies = []
+        fscores = []
+        while data_size <= len(self.feature_set):
+            curr_data_set = self.feature_set[:data_size]
+            curr_size = int(len(curr_data_set) * 0.8)
+            train_set = curr_data_set[:curr_size]
+            test_set = curr_data_set[curr_size:]
+            
+            self.train(False, train_set)
+            accuracy, fscore = self.test(test_set)
+
+            accuracies.append(accuracy)
+            fscores.append(fscore)
+            data_sizes.append(data_size)
+            data_size += inc_size
+
+        plt.plot(data_sizes, accuracies, label='Accuracy', linewidth=3)
+        plt.plot(data_sizes, fscores, label='F1-score', linewidth=3)
+        plt.title('Learning Curves')
+        plt.xlabel('Dataset Size')
+        plt.legend(loc='lower right')
+        plt.show()
